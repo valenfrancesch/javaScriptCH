3ca742de830bdc3ff5dd59bf5cb8b890a44ca31e
valenfrancesch <valufrancesch@gmail.com>
2023-07-31 11:07:02 -0300

    Mejoras en el js

--- script.js
+++ script.js
@@ -1,54 +1,73 @@
-let dia = parseInt(prompt("Escriba el número del día en el que se encuentra: Lunes(1), Martes(2), Miércoles(3), Jueves(4), Viernes(5), Sábado(6), Domingo(7)"));
-let diaSemana ="";
+let todosDias = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
 
-switch(dia){
-    case 1:
-        diaSemana = "Lunes";
-        break;
-
-    case 2:
-        diaSemana = "Martes";
-        break;
-    
-    case 3:
-        diaSemana = "Miércoles";
-        break;
-    
-    case 4:
-        diaSemana = "Jueves";
-        break;
-    
-    case 5:
-        diaSemana = "Viernes";
-        break;
-    
-    case 6:
-        diaSemana = "Sábado";
-        break;
-    
-    case 7:
-        diaSemana = "Domingo";
-        break;
-    
-    default:
-        diaSemana = "Hoy";
-        break;
+function guardarTarea(txt, dia){
+    let tarea = {
+        tarea: txt,
+        dia: todosDias[dia-1],
+        diaNum: dia,
+    }
+    return tarea;
 }
 
-let ingresoTarea = true;
-let i = 0;
-while (true){
-    let verificador = parseInt(prompt("Escriba 1 si quiere ingresar una tarea para hoy. De lo contrario, escriba 2"));
-    if(verificador == 2){
-        ingresoTarea = false;
-        if(i == 0){
-            console.log(diaSemana + ", no hay nada por hacer");
-        }else{
-            console.log("Eso es todo por hoy!");
+function ingresarDia(){
+    let dia = parseInt(prompt("Escriba el número del día en el que quiere realizar la tarea: Lunes(1), Martes(2), Miércoles(3), Jueves(4), Viernes(5), Sábado(6), Domingo(7)"));
+    if(dia > 7 || dia < 1){
+        alert("Error. Escriba un número entre el 1 y el 7");
+        dia = ingresarDia();
+    }
+    return dia;
+}
+
+function ingresarTarea(){
+    let tarea= prompt("Escriba la tarea pendiente");
+    return tarea;
+}
+
+function ingresoCaso(){
+    let verificador = parseInt(prompt("Escriba 1 si quiere ingresar una tarea. De lo contrario, escriba 2"));
+    if(verificador == 1){
+        return 1;
+    }else if(verificador == 2){
+        return 0;
+    }else{
+        alert("Error. Ingrese el número 1 o 2");
+        return -1;
+    }
+}
+
+function ordenarTareas(listaTareas){
+    listaTareas.sort((a, b) => (a.diaNum > b.diaNum) ? 1 : (a.diaNum < b.diaNum) ? -1 : 0);
+    return listaTareas;
+}
+
+function mostrarTareas(listaTareas){
+    let size = listaTareas.length;
+    if(size == 0){
+        console.log("No hay ninguna tarea");
+        return;
+    }
+
+    let dia = 0;
+    for(let i=0; i<size; i++){
+        if (listaTareas[i].diaNum != dia){
+            if(dia != 0){
+                console.log("-----------------------");
+            }
+            console.log("Tareas del día " + listaTareas[i].dia);
+            dia = listaTareas[i].diaNum;
         }
+        console.log("- " + listaTareas[i].tarea);
+    }
+}
+
+let tareas = [];
+
+while (true){
+    verificador = ingresoCaso();
+    if(verificador == 1){
+        tareas.push(guardarTarea(ingresarTarea(), ingresarDia()));
+    }else if(verificador == 0){
+        mostrarTareas(ordenarTareas(tareas));
         break;
     }
-    let tarea= prompt("Escriba la tarea que quiere hacer hoy");
-    console.log(diaSemana + ": " + tarea);
-    i++;
 }
